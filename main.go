cf2dddb164d8fd1f86acfebb81c898b6ed7f3290
Rashed Kamal <rashedkamal@Rasheds-MBP.attlocal.net>
2020-08-20 23:47:33 -0400

    Pointer Example

--- main.go
+++ main.go
@@ -6,4 +6,25 @@ import (
 
 func main() {
 	fmt.Println("Hello World")
+
+	tRate := 4.75
+	fmt.Println("Current rate", tRate)
+
+	//pointer examples
+	var testString *string = new(string)
+
+	*testString = "my current pointer"
+	fmt.Println(*testString, testString)
+
+	*testString = "hey new string"
+	fmt.Println(*testString, testString)
+
+	something := "I am not pointer"
+
+	ptr := &something
+	fmt.Println(*ptr, ptr)
+
+	something = "I am a pointer now"
+	fmt.Println(*ptr, ptr)
+
 }
