7c00dbb2407a61d2f668e5aabbda91fc50472bc5
RK-GCP <rashed.kamal@live.com>
2020-08-30 00:59:51 -0400

    Updated Model

--- models/user.go
+++ models/user.go
@@ -1,5 +1,10 @@
 package models
 
+import (
+	"errors"
+	"fmt"
+)
+
 //User type
 type User struct {
 	ID        int
@@ -19,8 +24,44 @@ func GetUsers() []*User {
 
 //AddUser to the collection
 func AddUser(u User) (User, error) {
+	if u.ID != 0 {
+		return User{}, errors.New("Newly created user id must be 0")
+	}
 	u.ID = nextID
 	nextID++
 	users = append(users, &u)
 	return u, nil
+}
+
+//GetUserByID - Returns User By Id
+func GetUserByID(id int) (User, error) {
+	for _, u := range users {
+		if u.ID == id {
+			return *u, nil
+		}
+	}
+
+	return User{}, fmt.Errorf("User Id '%v' not found", id)
+}
+
+//UpdateUser - updates a User
+func UpdateUser(u User) (User, error) {
+	for indx, candidate := range users {
+		if candidate.ID == u.ID {
+			users[indx] = &u
+			return u, nil
+		}
+	}
+	return User{}, fmt.Errorf("User with Id '%v' not found", u.ID)
+}
+
+//RemoveUserByID - removes a user by Id
+func RemoveUserByID(id int) error {
+	for indx, candidate := range users {
+		if candidate.ID == id {
+			users = append(users[:indx], users[indx+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("User with Id '%v' not found", id)
 }
